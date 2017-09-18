bd4d496cbc2022f15ec158e86ed62be0f24231b3
teresa.gagliuffi <teresa.gagliuffi@bbva.com>
2017-09-18 17:42:50 -0500

    tgagliuffi

---
+++ findim-dom/src/main/java/com/bbva/findim/dom/common/ConstantResponseMessage.java
@@ -0,0 +1,27 @@
+package com.bbva.findim.dom.common;
+
+public class ConstantResponseMessage {
+	
+	public static final String CODE_RPTA_OK = "0000";
+	public static final String CODE_RPTA_ERROR = "9999";
+	
+	public static final String CODE_RPTA_ERROR_DESCRIP = "Sucedio un Error inesperado.";
+	public static final String MJS_ERROR_FILTRO = "ERROR EN SW FILTRO";
+	public static final String MSJ_EXCEPTION = "Error. Comuníquese con sistemas.";
+	
+	
+	
+	public static final String MSJ_OK_CUSTOMER = "SE ENCONTRO CLIENTE";
+	public static final String MSJ_OK_PERSON = "SE ENCONTRO PERSONA";
+
+	public static final String MSJ_OK_PROPOSALES = "SE ENCONTRO PROPUESTA";
+		public static final String MSJ_OK_ALTAPROP_UNO = "OK :Cliente: ";
+		public static final String MSJ_OK_APTAPROP_DOS = "- Contrato: ";
+		
+	public static final String	RESP_SERV_200	= "200";
+	public static final String	RESP_SERV_201	= "201";	
+	public static final String	RESP_SERV_400	= "400";	
+	public static final String	RESP_SERV_409	= "409";	
+	public static final String	RESP_SERV_500	= "500";	
+
+}
