e54dc1c0676e7cf354c335ab9feed7eca5e312b6
teresa.gagliuffi <teresa.gagliuffi@bbva.com>
2017-10-11 10:48:47 -0500

    tgagliuffi

---
+++ findim-dom/src/main/java/com/bbva/findim/dom/UbigeoBean.java
@@ -0,0 +1,71 @@
+package com.bbva.findim.dom;
+
+public class UbigeoBean {
+	 
+	 private Long idUbigeo;
+	 private String cdDpto;
+	 private String nbDpto;
+	 private String cdProv;
+	 private String nbProv;
+	 private String cdDist;
+	 private String nbDist;
+	 private String ubgReniec;
+	 private String ubgHost;
+	 
+	public Long getIdUbigeo() {
+		return idUbigeo;
+	}
+	public void setIdUbigeo(Long idUbigeo) {
+		this.idUbigeo = idUbigeo;
+	}
+	public String getCdDpto() {
+		return cdDpto;
+	}
+	public void setCdDpto(String cdDpto) {
+		this.cdDpto = cdDpto;
+	}
+	public String getNbDpto() {
+		return nbDpto;
+	}
+	public void setNbDpto(String nbDpto) {
+		this.nbDpto = nbDpto;
+	}
+	public String getCdProv() {
+		return cdProv;
+	}
+	public void setCdProv(String cdProv) {
+		this.cdProv = cdProv;
+	}
+	public String getNbProv() {
+		return nbProv;
+	}
+	public void setNbProv(String nbProv) {
+		this.nbProv = nbProv;
+	}
+	public String getCdDist() {
+		return cdDist;
+	}
+	public void setCdDist(String cdDist) {
+		this.cdDist = cdDist;
+	}
+	public String getNbDist() {
+		return nbDist;
+	}
+	public void setNbDist(String nbDist) {
+		this.nbDist = nbDist;
+	}
+	public String getUbgReniec() {
+		return ubgReniec;
+	}
+	public void setUbgReniec(String ubgReniec) {
+		this.ubgReniec = ubgReniec;
+	}
+	public String getUbgHost() {
+		return ubgHost;
+	}
+	public void setUbgHost(String ubgHost) {
+		this.ubgHost = ubgHost;
+	}
+	 
+	 
+}
