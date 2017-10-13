486f753418f77d5d52ed77dc053006dd0303d812
ppazos <percy1409@gmail.com>
2017-10-13 11:59:05 -0500

    reporte

--- findim-sql/src/main/java/com/bbva/findim/sql/dao/LogAltaContratoDao.java
+++ findim-sql/src/main/java/com/bbva/findim/sql/dao/LogAltaContratoDao.java
@@ -1,11 +1,16 @@
 package com.bbva.findim.sql.dao;
 
+import java.util.List;
+
 import com.bbva.findim.dom.AltaContratoRequest;
+import com.bbva.findim.dom.LogContratoBean;
 import com.bbva.findim.dom.RespuestaBean;
 
 public interface LogAltaContratoDao {
 
 	Long insert(AltaContratoRequest input);
 	Integer update(Long id, RespuestaBean output);
+	
+	List<LogContratoBean> obtenerClientesAprobadosRechazadosMes(String fhInicio,String fhFin);
 
 }
--- findim-sql/src/main/java/com/bbva/findim/sql/service/LogAltaContratoService.java
+++ findim-sql/src/main/java/com/bbva/findim/sql/service/LogAltaContratoService.java
@@ -2,12 +2,17 @@ package com.bbva.findim.sql.service;
 
 
 
+import java.util.List;
+
 import com.bbva.findim.dom.AltaContratoRequest;
+import com.bbva.findim.dom.LogContratoBean;
 import com.bbva.findim.dom.RespuestaBean;
 
 public interface LogAltaContratoService {
 
 	Long insert(AltaContratoRequest input);
 	Integer update(Long id, RespuestaBean output);
+	
+	List<LogContratoBean> listaClientesAprobadosDesaprobados(String inicio,String fin);
 
 }
