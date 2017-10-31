4eccde26d536fcffc679e4b342ffbd2fb28b43db
teresa.gagliuffi <teresa.gagliuffi@bbva.com>
2017-10-31 13:52:01 -0500

    tgagliuffi

---
+++ findim-dom/src/main/java/com/bbva/findim/dom/File.java
@@ -0,0 +1,17 @@
+package com.bbva.findim.dom;
+
+public class File {
+	
+	private String ruta;
+
+	public String getRuta() {
+		return ruta;
+	}
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+	
+	
+
+}
